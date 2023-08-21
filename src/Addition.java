22449865ec18cc2cb8fa68a6a5b251bb77d9448e
Alan <140322633+arthenux@users.noreply.github.com>
2023-08-21 12:50:29 +0100

    Added user input read

--- src/Addition.java
+++ src/Addition.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Addition {
 
     public static void main(String[] args) {
@@ -8,5 +10,8 @@ public class Addition {
         int number1;
         int number2;
         int sum;
+
+        firstNumber = JOptionPane.showInputDialog("Enter first number");
+        secondNumber = JOptionPane.showInputDialog("Enter second number");
     }
 }
